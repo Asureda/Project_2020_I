29ce65e58e510aa2186d328b5093272f2ad1a71d
ocabanas <61125318+ocabanas@users.noreply.github.com>
2020-02-27 15:14:05 +0100

    Update (#1)
    * Update elena.f90
    
    * Modul Inicialitzar
    
    * Modul Andersen
    
    * Modul Distribucio Radial
    
    * Modul Reescala
    
    * Full_2_Modul_Distrib_Radial.f90
    
    * Mòdul Reescala amb funció cinètica
    
    * Update config.dat
    
    * Update parameters.dat
    
    * moduls_def
    
    * moduls_def
    
    Co-authored-by: elenaricart13 <61150228+elenaricart13@users.noreply.github.com>
    Co-authored-by: Asureda <61150333+Asureda@users.noreply.github.com>

---
+++ PROGRAM/PBC.f90
@@ -0,0 +1,25 @@
+! Modul de les condicions periodiques de contorn
+MODULE PBC
+use READ_DATA
+implicit none
+contains
+FUNCTION PBC1(x,L)
+!FUNCTION THAT RETURNS THE DISTANCE GIVEN PBC AND L
+    IMPLICIT NONE
+    REAL*8 x,L,PBC1
+    PBC1=x-int(2d0*x/L)*L
+    RETURN
+END FUNCTION
+FUNCTION PBC2(x,L)
+!FUNCTION THAT RETURNS THE DISTANCE GIVEN PBC AND L
+    IMPLICIT NONE
+    REAL*8 x,L,PBC2
+    IF(x.lt.0)THEN
+        x=L+mod(x,L)
+    ELSE IF(x.gt.L) THEN
+        x=mod(x,L)
+    END IF
+    PBC2=x
+    RETURN
+END FUNCTION
+END MODULE PBC
