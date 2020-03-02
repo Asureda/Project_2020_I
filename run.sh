02b3bab1b9680d5bda6b52692cdba4ce890179a1
Oriol_C <oriol.cabanas@gmail.com>
2020-03-02 12:29:29 +0100

    Fixed running errors

--- run.sh
+++ run.sh
@@ -1,8 +1,8 @@
 #Execute file
 echo "Deleting Data files from PROGRAM"
-rm /PROGRAM/*.dat
+rm PROGRAM/*.dat
 echo "Copy files from INPUT to PROGRAM"
-cp /INPUT/*.dat /PROGRAM/
+cp INPUT/*.dat PROGRAM/
 echo "Inside /PROGRAM"
 cd PROGRAM/
 echo "Executing MakeFile"
@@ -15,11 +15,11 @@ echo "From bash. Main program ended"
 echo "Getting out from PROGRAM/"
 cd ..
 echo "Moving files from PROGRAM to OUTPUT"
-mv /PROGRAM/thermodynamics_reduced.dat /OUTPUT/thermodynamics_reduced.dat
-mv /PROGRAM/thermodynamics_real.dat /OUTPUT/thermodynamics_real.dat
-mv /PROGRAM/distriv_funct.dat /OUTPUT/distriv_funct.dat
-mv /PROGRAM/positions.xyz /OUTPUT/positions.xyz
+mv PROGRAM/thermodynamics_reduced.dat OUTPUT/thermodynamics_reduced.dat
+mv PROGRAM/thermodynamics_real.dat OUTPUT/thermodynamics_real.dat
+mv PROGRAM/distriv_funct.dat OUTPUTdistriv_funct.dat
+mv PROGRAM/positions.xyz OUTPUT/positions.xyz
 echo "Deleting all data files from PROGRAM"
-rm /PROGRAM/*.dat
+rm PROGRAM/*.dat
 echo "From bash. End of script"
 
