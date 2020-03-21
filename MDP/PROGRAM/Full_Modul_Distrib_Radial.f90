b7a45876983ccf084e4c26cb6aad2c4d788dc587
Elena <elenaricart13@gmail.com>
2020-03-21 10:27:30 +0100

    MPI_Distribucio_Radial

--- MDP/PROGRAM/Full_Modul_Distrib_Radial.f90
+++ MDP/PROGRAM/Full_Modul_Distrib_Radial.f90
@@ -3,6 +3,7 @@ module Distribucio_Radial
 use READ_DATA
 !Cridem el mòdul PBC definit  a l'arxiu PBC
 use PBC
+use matrix
 
 IMPLICIT NONE
 
@@ -13,21 +14,28 @@ contains
     IMPLICIT NONE
     INTEGER i,coef,j
     REAL*8 dist,vec(:), r(:,:),dx,dy,dz
-    DO i=1,n_particles
-        DO j=1,n_particles
-            IF (i.ne.j) THEN
-                !Calculem el diferencial en l'espai de les tres dimensions sobre les que treballem
-                dx=PBC1(r(j,1)-r(i,1),L)
-                dy=PBC1(r(j,2)-r(i,2),L)
-                dz=PBC1(r(j,3)-r(i,3),L)
-                dist=(dx**2d0+dy**2d0+dz**2d0)**0.5
-                coef=int(0.5+dist/dx_radial)
-                IF ((coef.gt.0).and.(coef.le.n_radial)) THEN
-                    vec(coef)=vec(coef)+1.0
-                END IF
-            END IF
-        END DO
-    END DO
+    !DO i=1,n_particles
+        !DO j=1,n_particles
+        !taskid= identificador del processador
+        IF (taskid.lt.n_working) THEN
+            DO i=simple_matrix(taskid,1),simple_matrix(taskid,2)
+                DO i=simple_matrix(taskid,3),simple_matrix(taskid,4)
+                    IF (i.ne.j) THEN
+                        !Calculem el diferencial en l'espai de les tres dimensions sobre les que treballem
+                        dx=PBC1(r(j,1)-r(i,1),L)
+                        dy=PBC1(r(j,2)-r(i,2),L)
+                        dz=PBC1(r(j,3)-r(i,3),L)
+                        dist=(dx**2d0+dy**2d0+dz**2d0)**0.5
+                        coef=int(0.5+dist/dx_radial)
+                        IF ((coef.gt.0).and.(coef.le.n_radial)) THEN
+                            vec(coef)=vec(coef)+1.0
+                        END IF
+                    END IF
+                END DO
+            END DO
+        END IF
+        MPI_BARRIER(comm , ierror)
+
     end subroutine RAD_DIST_INTER
 
     subroutine RAD_DIST_FINAL(vec,n_gr_meas)
@@ -38,12 +46,19 @@ contains
     REAL*8 vec(:),result(0:n_radial+1),aux
     vec=vec/(1d0*n_gr_meas*n_particles)
     result=0d0
-    DO i=2,n_radial
-        aux=(density*4d0*3.1415*((((i)*dx_radial)**3d0)-(((i-1)*dx_radial)**3d0)))/3d0
-        result(i)=vec(i)/aux
-    END DO
+    !DO i=2,n_radial
+    !taskid= identificador del processador
+    IF (taskid.lt.n_working) THEN
+        DO i=simple_matrix(taskid,1) + 2 , simple_matrix(taskid,2)
+            aux=(density*4d0*3.1415*((((i)*dx_radial)**3d0)-(((i-1)*dx_radial)**3d0)))/3d0
+            result(i)=vec(i)/aux
+        END DO
+    END IF
+    MPI_BARRIER(comm , ierror)
+
     vec=0d0
-    vec=result 
+    vec=result
+
     end subroutine RAD_DIST_FINAL
 
 end module Distribucio_Radial
