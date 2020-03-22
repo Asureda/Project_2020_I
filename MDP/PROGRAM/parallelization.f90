44b68bc0acc6e4da9d46a67fdd490bf6ee9e7b58
Oriol_C <oriol.cabanas@gmail.com>
2020-03-22 11:47:50 +0100

    Init paralel

---
+++ MDP/PROGRAM/parallelization.f90
@@ -0,0 +1,14 @@
+module parallel_routines
+    use read_vars
+    IMPLICIT NONE
+
+    contains
+    SUBROUTINE simple_loop_matrix()
+        IMPLICIT NONE
+
+    END SUBROUTINE
+
+    SUBROUTINE doble_loop_matrix()
+        IMPLICIT NONE
+    END SUBROUTINE
+END module
