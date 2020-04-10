4f723be3b37e070ec0ca653fd3dbd54f883b0d53
Oriol Cabanas <oriol.cabanas@gmail.com>
2020-04-10 17:21:16 +0200

    Sequencial arreglat

--- MDS/PROGRAM/verlet.f90
+++ MDS/PROGRAM/verlet.f90
@@ -22,16 +22,13 @@ SUBROUTINE VELO_VERLET(r,v,F)
         !v(i,:)=v(i,:)+5d-1*F(i,:)*h
     !END DO
     !taskid= identificador del processador
-    IF (taskid.lt.n_working) THEN
-        DO i=simple_matrix(taskid,1),simple_matrix(taskid,2)
-            r(i,:)=r0(i,:)+v0(i,:)*h+5d-1*F0(i,:)*h*h
-            !r(i,:)=r(i,:)+v(i,:)*h+5d-1*F(i,:)*h*h
-            r(i,1)=PBC2(r(i,1),L)
-            r(i,2)=PBC2(r(i,2),L)
-            r(i,3)=PBC2(r(i,3),L)
-        END DO
-    END IF
-    MPI_BARRIER(comm , ierror)
+    DO i=1,n_particles
+        r(i,:)=r0(i,:)+v0(i,:)*h+5d-1*F0(i,:)*h*h
+        !r(i,:)=r(i,:)+v(i,:)*h+5d-1*F(i,:)*h*h
+        r(i,1)=PBC2(r(i,1),L)
+        r(i,2)=PBC2(r(i,2),L)
+        r(i,3)=PBC2(r(i,3),L)
+    END DO
 
     CALL INTERACTION_CUTOFF(r,F,cutoff)
     kinetic=0d0
