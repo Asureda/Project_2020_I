ecd53b8ce17489040b35dc6ce03c4d59e7c75ced
elenaricart13 <61150228+elenaricart13@users.noreply.github.com>
2020-04-15 20:09:01 +0200

    vel_inicial.f90
    vel inicials comentat

--- MDS/PROGRAM/vel_initial.f90
+++ MDS/PROGRAM/vel_initial.f90
@@ -1,10 +1,23 @@
-! Modul per la distribucio uniforme de les velocitats
+!GRUP I: Àlex, Oriol, Laia, Sílvia i Elena
+
 MODULE Distribucio_Uniforme_vel
+
 use READ_DATA
 use Reescala_velocitats
+
 implicit none
+
 contains
+
 SUBROUTINE UNIFORM_VELO(v,T)
+
+    !OBJECTIU: Aconseguir una distribució uniforme de les velocitats
+    !          Després, a partir de la subrutina del reescalat de velocitats (VELO_RESCALING), fer - li el reescalat a una T.
+    
+    !INPUTS: Matriu de velocitats(v), temperatura(T)
+    
+    !OUTPUTS: Matriu de velocitats(v)
+
     INTEGER :: i,j,seed
     REAL*8 :: v(:,:),vi,vtot,T    
     seed=13
@@ -21,7 +34,7 @@ SUBROUTINE UNIFORM_VELO(v,T)
         v(n_particles,i)=-vtot
      END DO
      
-     !Resacling the velocities to the temperature
+     !Fem el reescalat a la temperatura marcada
      CALL VELO_RESCALING(v,T)
      RETURN
 END SUBROUTINE UNIFORM_VELO
