c3ab785a8d871ef8d70001db7ef610d2892ae82d
elenaricart13 <61150228+elenaricart13@users.noreply.github.com>
2020-04-15 19:57:27 +0200

    PBC.f90
    condicions de contorn comentat

--- MDS/PROGRAM/PBC.f90
+++ MDS/PROGRAM/PBC.f90
@@ -1,25 +1,53 @@
-! Modul per les condicions periodiques de contorn
-MODULE PBC
+!GRUP I: Àlex, Oriol, Laia, Sílvia i Elena
+module PBC
+
 use READ_DATA
+
 implicit none
+
 contains
 
 FUNCTION PBC1(x,L)
+
+    !OBJECTIU: Definir les condicions periòdiques de contorn per tal de tenir les partícules
+    !           definides sempre en una caixa i no variar així el nombre de partícules de la nostra simulació.
+    
+    !INPUTS: posicions(x), costat de la caixa (L)
+    
+    !OUTPUTS: posicions(x)
+    
     IMPLICIT NONE
+    
     REAL*8 x,L,PBC1
+    
     PBC1=x-int(2d0*x/L)*L
+    
     RETURN
 END FUNCTION
 
 FUNCTION PBC2(x,L)
+
+    !OBJECTIU: Definir les condicions periòdiques de contorn per tal de tenir les partícules
+    !           definides sempre en una caixa i no variar així el nombre de partícules de la nostra simulació.
+    
+    !INPUTS: posicions(x), costat de la caixa (L)
+    
+    !OUTPUTS: posicions(x)
+    
     IMPLICIT NONE
+    
     REAL*8 x,L,PBC2
+    
     IF(x.lt.0)THEN
         x=L+mod(x,L)
+    
     ELSE IF(x.gt.L) THEN
         x=mod(x,L)
+    
     END IF
+    
     PBC2=x
+    
     RETURN
 END FUNCTION
 
