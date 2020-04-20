ee2cb94563d96f9965e1206a9a9e1b23f8735ae5
Asureda <61150333+Asureda@users.noreply.github.com>
2020-04-20 15:37:16 +0200

    Update Full_modul_Inicialitzar.f90

--- MDP-Pair/PROGRAM/Full_modul_Inicialitzar.f90
+++ MDP-Pair/PROGRAM/Full_modul_Inicialitzar.f90
@@ -7,12 +7,10 @@ IMPLICIT NONE
 contains
 
     subroutine FCC_Initialize(r)
-    !En aquesta primera subrutina, definirem les posicions inicials de la nostra configuració.
+    ! We create an FCC latice of a system of N particles
     INTEGER :: n,i,j,k
     REAL*8 :: r(:,:)
     n=1
-    !print*,n
-    !Definim aquesta configuració en les tres dimensions de l'espai
     if (taskid==0) then
       DO i=0,M-1
           DO j=0,M-1
@@ -27,16 +25,12 @@ contains
               END DO
           END DO
       END DO
-    !  PRINT*, 'particles positioned', n-1, 'of a total imput', n_particles
+
   end if
-      ! DO k= 1,3
-      !   call MPI_BCAST(r(:,k), n_particles, MPI_DOUBLE_PRECISION, 0, MPI_COMM_WORLD, IERROR)
-      ! END DO
     end subroutine FCC_Initialize
 
     subroutine Uniform_velocity(v)
-    !Aquesta subrutina, ens permetrà seguir una distribució normal al crear la matriu de les velocitats de la simulació.
-    !Sempre es farà a una temperatura concreta
+   ! We initialize the velocities for a system of N particles in a uniform distribution
     INTEGER :: i,j,seed !,k
     REAL*8 :: v(:,:),vi,vtot
     !seed=13
@@ -54,24 +48,18 @@ contains
             v(n_particles,i)=-vtot
           END DO
     end if
-    ! DO k= 1,3
-    !   call MPI_BCAST(v(:,k), n_particles, MPI_DOUBLE_PRECISION, 0, MPI_COMM_WORLD, IERROR)
-    ! END DO
+
     RETURN
     end subroutine Uniform_velocity
     subroutine Velo_Rescaling_mod(v,T)
-    !Amb aquesta subrutina trobarem la velocitat, un cop hem reescalat, a una temperatura i un número de partícules concret
+    ! We rescale the velocities so the kinetic energy is consistent with the temperature
     IMPLICIT NONE
-    !INTEGER :: k
     REAL*8 v(:,:),T,alpha,KINETIC_LOC
     if (taskid==0) then
         KINETIC_LOC=KINETIC_ENERGY(v)
         alpha=sqrt(3d0*n_particles*T/(2d0*KINETIC_LOC))
         v=alpha*v
     end if
-    ! DO k= 1,3
-    !   call MPI_BCAST(v(:,k), n_particles, MPI_DOUBLE_PRECISION, 0, MPI_COMM_WORLD, IERROR)
-    ! END DO
     end subroutine Velo_Rescaling_mod
 
 end module Inicialitzar
