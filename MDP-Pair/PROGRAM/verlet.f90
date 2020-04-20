721bc01898648aeef0d25900afc21e77a61bf870
Asureda <61150333+Asureda@users.noreply.github.com>
2020-04-20 16:03:12 +0200

    Update verlet.f90

--- MDP-Pair/PROGRAM/verlet.f90
+++ MDP-Pair/PROGRAM/verlet.f90
@@ -33,25 +33,6 @@ SUBROUTINE VELO_VERLET(r,v,F)
     DO i=index_matrix(taskid+1,1), index_matrix(taskid+1,2)
         v(i,:)=v(i,:)+5d-1*(F(i,:))*h
     END DO
-   ! DO k=1,3
-   !  ! CALL MPI_ALLGATHERV(v(index_matrix(taskid+1,1):index_matrix(taskid+1,2),k),&
-   !  !                     & (index_matrix(taskid+1,2)-index_matrix(taskid+1,1)+1),MPI_DOUBLE_PRECISION, &
-   !  !                     & v(:,k),num_send,desplac,MPI_DOUBLE_PRECISION,MPI_COMM_WORLD,ierror)
-   !  CALL MPI_GATHERV(v(index_matrix(taskid+1,1):index_matrix(taskid+1,2),k),&
-   !                       & (index_matrix(taskid+1,2)-index_matrix(taskid+1,1)+1),MPI_DOUBLE_PRECISION, &
-   !                       & v(:,k),num_send,desplac,MPI_DOUBLE_PRECISION,0,MPI_COMM_WORLD,ierror)
-   !  END DO
-
-      !  IF(taskid==0) then
-      !    DO i = 1,n_particles
-      !      kinetic=kinetic+5d-1*(v(i,1)**2d0+v(i,2)**2d0+v(i,3)**2d0)
-      !   end do
-      ! end if
-     ! DO i = index_matrix(taskid+1,1), index_matrix(taskid+1,2)
-     !       kinetic=kinetic+5d-1*(v(i,1)**2d0+v(i,2)**2d0+v(i,3)**2d0)
-     !    end do
-     ! call MPI_REDUCE(kinetic,kinetic,1,MPI_REAL,MPI_SUM,0,MPI_COMM_WORLD,ierror) !(No funciona be)
-
-
+   
 END SUBROUTINE
 END MODULE Verlet_Algorithm
