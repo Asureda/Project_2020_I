7c61b31a285bedff381939288ab3cf564d3ead6e
ocabanas <61125318+ocabanas@users.noreply.github.com>
2020-04-20 19:40:14 +0200

    Update main.f90

--- MDS1/PROGRAM/main.f90
+++ MDS1/PROGRAM/main.f90
@@ -9,7 +9,9 @@ PROGRAM SEQUENTIAL_MD
   use SAMPLE
   IMPLICIT NONE
   real*8 :: starttime, endtime
-  
+!##########################
+! INITIAL CONFIGURATION
+!#########################  
   call read_all_data()
   call other_global_vars()
   call INITIALIZE_VARS()
@@ -17,7 +19,9 @@ PROGRAM SEQUENTIAL_MD
   call Uniform_velocity(v)
   call VELO_RESCALING_MOD(v,T_therm_prov)
   call cpu_time(starttime)
-
+!##########################
+! MELING AND THERMALIZATION
+!#########################
    DO nstep=1,n_melting
     call velo_verlet(r,v,F)
     call andersen(v,T_therm_prov)
@@ -31,15 +35,23 @@ PROGRAM SEQUENTIAL_MD
   open(46,file='temp_pres_real.dat')
   open(53,file='distrib_funct.dat')
   open(54,file='positions.xyz')
-
+!##########################
+! VERLET EVOLUTION
+!#########################
   DO nstep=1,n_verlet
     t=t_a+nstep*h
+    !Verlet step
     call VELO_VERLET(r,v,F)
     if(is_thermostat.eqv..true.)THEN
       call andersen(v,T_therm)
     end if
+  !Computing the observables
   CALL SAMPLES()
   END DO
+!##########################
+! FINAL COMPUTATIONS
+!#########################
+  !Final form of the gdr
   CALL gdr()
   ! put code to test here
 call cpu_time(endtime)
